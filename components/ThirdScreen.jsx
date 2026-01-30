87449619a9fc3354b7f4988ca8d92194d789dea7
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-01-30 22:31:22 +0530

    Complete 1st mvp third screen

--- components/ThirdScreen.jsx
+++ components/ThirdScreen.jsx
@@ -13,6 +13,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
 const outfit = Outfit({
@@ -44,37 +45,66 @@ const ThirdPage = () => {
                 value="overview"
                 className={`w-[300px] !h-24 !rounded-lg ${outfit.className} text-2xl`}
               >
-                <div className="w-full px-4"><h5 className={`${outfit.className} text-left text-lg tracking-tight`}>Bank accounts</h5><p className={`${outfit.className} text-left text-sm`}>Safely connect to your all<br></br>your bank accounts.</p></div>
+                <div className="w-full px-4">
+                  <h5
+                    className={`${outfit.className} text-left text-lg tracking-tight`}
+                  >
+                    Bank accounts
+                  </h5>
+                  <p className={`${outfit.className} text-left text-sm`}>
+                    Safely connect to your all<br></br>your bank accounts.
+                  </p>
+                </div>
               </TabsTrigger>
               <TabsTrigger
                 value="analytics"
                 className={`w-[300px] !h-24 !rounded-lg ${outfit.className} text-2xl`}
               >
-                <div className="w-full px-4"><h5 className={`${outfit.className} text-left text-lg tracking-tight`}>Auto-categorised</h5><p className={`${outfit.className} text-left text-sm`}>All your transactions will be<br></br>safely sorted instantly.</p></div>
+                <div className="w-full px-4">
+                  <h5
+                    className={`${outfit.className} text-left text-lg tracking-tight`}
+                  >
+                    Auto-categorised
+                  </h5>
+                  <p className={`${outfit.className} text-left text-sm`}>
+                    All your transactions will be<br></br>safely sorted
+                    instantly.
+                  </p>
+                </div>
               </TabsTrigger>
               <TabsTrigger
                 value="reports"
                 className={`w-[300px] !h-24 !rounded-lg ${outfit.className} text-2xl`}
               >
-               <div className="w-full px-4"><h5  className={`${outfit.className} text-left text-lg tracking-tight`}>Smart tips by AI</h5><p className={`${outfit.className} text-left text-sm`}>Get AI-generated tips  <br></br>tailored to your habits.</p></div>
+                <div className="w-full px-4">
+                  <h5
+                    className={`${outfit.className} text-left text-lg tracking-tight`}
+                  >
+                    Smart tips by AI
+                  </h5>
+                  <p className={`${outfit.className} text-left text-sm`}>
+                    Get AI-generated tips <br></br>tailored to your habits.
+                  </p>
+                </div>
               </TabsTrigger>
             </TabsList>
-            <TabsContent value="overview" >
+            <TabsContent value="overview">
               <div className="h-[400px] rounded-3xl border bg-card text-card-foreground shadow-sm">
                 <div className="flex h-full justify-center items-center p-8 bg-amber-300">
                   <div className="flex w-full h-full flex-col items-start py-2 bg-violet-300">
-                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                    <h3
+                      className={`${outfit.className} tracking-tight text-xl `}
+                    >
                       Filter by <br></br>category & time
                     </h3>
-
                   </div>
                   <div className="flex w-full h-full flex-col items-start py-2 bg-violet-900">
-                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                    <h3
+                      className={`${outfit.className} tracking-tight text-xl `}
+                    >
                       Filter by <br></br>category & time
                     </h3>
-
                   </div>
-
                 </div>
                 <div className="p-6 pt-0 text-muted-foreground text-sm">
                   You have 12 active projects and 3 pending tasks.
@@ -85,18 +115,70 @@ const ThirdPage = () => {
               <div className="h-[400px] rounded-3xl border bg-card text-card-foreground shadow-sm">
                 <div className="flex h-full justify-center items-center p-8 bg-amber-300">
                   <div className="flex !pl-10 !pt-8 relative w-[50%] h-full flex-col items-start py-2 bg-violet-300">
-                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                    <h3
+                      className={`${outfit.className} font-medium tracking-tight text-xl `}
+                    >
                       Filter by <br></br>category & time
                     </h3>
-
                   </div>
-                  <div className="flex w-full pt-8 h-full flex-col items-start bg-violet-900">
-                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
-                      Filter by <br></br>category & time
-                    </h3>
-
+                  <div className="flex w-full pt-8 h-full flex-col items-start bg-violet-900 gap-4">
+                    <div className="w-full h-full flex items-center justify-center">
+                      <div id="list" className="flex justify-start gap-4 items-center w-full px-4">
+                        <div className="w-[76px] h-[76px] bg-white rounded-xl flex items-center justify-center">
+                          <Image
+                            src="/starbucks.jpg"
+                            alt="Starbucks logo"
+                            width={200}
+                            height={200}
+                            className="rounded-lg w-10 h-10 object-cover"
+                          />
+                        </div>
+                        <div className={`flex-1 ${outfit.className} font-medium tracking-tight`}>
+                          <h2 className="text-lg leading-6 text-white">Starbucks</h2>
+                          <p className="text-sm text-white/80">Food &nbsp; & &nbsp; Drinks &nbsp;&nbsp;&nbsp;  May 22</p>
+                        </div>
+                        <div className={`${outfit.className} font-medium tracking-tight`}>
+                          <p className="text-lg text-white">-$6.80</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="w-full h-full flex items-center justify-center"><div id="list" className="flex justify-start gap-4 items-center w-full px-4">
+                        <div className="w-[76px] h-[76px] bg-white rounded-xl flex items-center justify-center">
+                          <Image
+                            src="/google.jpg"
+                            alt="Starbucks logo"
+                            width={200}
+                            height={200}
+                            className="rounded-lg w-10 h-10 object-cover"
+                          />
+                        </div>
+                        <div className={`flex-1 ${outfit.className} font-medium tracking-tight`}>
+                          <h2 className="text-lg leading-6 text-white">May salary</h2>
+                          <p className="text-sm text-white/80">Income &nbsp;&nbsp;&nbsp;  May 20</p>
+                        </div>
+                        <div className={`${outfit.className} font-medium tracking-tight`}>
+                          <p className="text-lg text-white">+$2,800.00</p>
+                        </div>
+                      </div></div>
+                    <div className="w-full h-full flex items-center justify-center "><div id="list" className="flex justify-start gap-4 items-center w-full px-4">
+                        <div className="w-[76px] h-[76px] bg-white rounded-xl flex items-center justify-center">
+                          <Image
+                            src="/netflix.jpg"
+                            alt="Starbucks logo"
+                            width={200}
+                            height={200}
+                            className="rounded-lg w-10 h-10 object-cover"
+                          />
+                        </div>
+                        <div className={`flex-1 ${outfit.className} font-medium tracking-tight`}>
+                          <h2 className="text-lg leading-6 text-white">Netflix</h2>
+                          <p className="text-sm text-white/80">Entertainment &nbsp;&nbsp;&nbsp;  May 19</p>
+                        </div>
+                        <div className={`${outfit.className} font-medium tracking-tight`}>
+                          <p className="text-lg text-white">-$13.99</p>
+                        </div>
+                      </div></div>
                   </div>
-
                 </div>
                 <div className="p-6 pt-0 text-muted-foreground text-sm">
                   Page views are up 25% compared to last month.
