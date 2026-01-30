591cc4b0e20e40004c79f38ff682568fc44f24c4
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-01-30 19:35:55 +0530

    Fix Layouting Issue:Tab

--- components/ThirdScreen.jsx
+++ components/ThirdScreen.jsx
@@ -28,49 +28,53 @@ const Bonny = localFont({
 
 const ThirdPage = () => {
   return (
-    <div className="container mt-6 h-screen bg-black">
-      <div className="flex max-w-5xl mx-auto px-4 sm:px-6 md:px-8 lg:px-10 ">
+    <div className="container mt-10 h-screen bg-black ">
+      <div className="flex max-w-4xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 mb-12 ">
         <h1
           className={`text-3xl sm:text-5xl md:text-6xl text-center font-medium tracking-tighter ${outfit.className} mt-4 text-white `}
         >
           Track everything you <br></br>need in your pocket.
         </h1>
       </div>
-      <div className="max-w-5xl mt-6 h-screen mx-auto px-4 sm:px-6 md:px-8 lg:px-10 ">
+      <div className="max-w-5xl mt-6 h-screen mx-auto px-4 sm:px-6 md:px-8 lg:px-12 ">
         <div className="dark">
-          <Tabs defaultValue="overview" className="max-w-4xl mx-auto h-[500px]">
-            <TabsList className="!h-20 !max-w-4xl !rounded-4xl mb-8">
+          <Tabs defaultValue="overview" className="max-w-3xl mx-auto h-[500px]">
+            <TabsList className="!h-20 !max-w-3xl !rounded-4xl mb-8">
               <TabsTrigger
                 value="overview"
-                className={`w-[300px] !h-16 !rounded-lg ${outfit.className} text-2xl`}
+                className={`w-[300px] !h-24 !rounded-lg ${outfit.className} text-2xl`}
               >
-                <div><h3>Bank accounts</h3><span className={`${outfit.className} text-sm`}>Safely connect to your all<br></br>your bank accounts.</span></div>
+                <div className="w-full px-4"><h5 className={`${outfit.className} text-left text-lg tracking-tight`}>Bank accounts</h5><p className={`${outfit.className} text-left text-sm`}>Safely connect to your all<br></br>your bank accounts.</p></div>
               </TabsTrigger>
               <TabsTrigger
                 value="analytics"
                 className={`w-[300px] !h-24 !rounded-lg ${outfit.className} text-2xl`}
               >
-                Analytics
+                <div className="w-full px-4"><h5 className={`${outfit.className} text-left text-lg tracking-tight`}>Auto-categorised</h5><p className={`${outfit.className} text-left text-sm`}>All your transactions will be<br></br>safely sorted instantly.</p></div>
               </TabsTrigger>
               <TabsTrigger
                 value="reports"
                 className={`w-[300px] !h-24 !rounded-lg ${outfit.className} text-2xl`}
               >
-                Reports
+               <div className="w-full px-4"><h5  className={`${outfit.className} text-left text-lg tracking-tight`}>Smart tips by AI</h5><p className={`${outfit.className} text-left text-sm`}>Get AI-generated tips  <br></br>tailored to your habits.</p></div>
               </TabsTrigger>
             </TabsList>
             <TabsContent value="overview" >
               <div className="h-[400px] rounded-3xl border bg-card text-card-foreground shadow-sm">
-                <div className="flex flex-col space-y-1.5 p-6">
-                  <h3 className="text-2xl font-semibold leading-none tracking-tight">
-                    Overview
-                  </h3>
-                  <p
-                    className={`${outfit.className} text-sm text-muted-foreground`}
-                  >
-                    View your key metrics and recent project activity. Track
-                    progress across all your active projects.
-                  </p>
+                <div className="flex h-full justify-center items-center p-8 bg-amber-300">
+                  <div className="flex w-full h-full flex-col items-start py-2 bg-violet-300">
+                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                      Filter by <br></br>category & time
+                    </h3>
+
+                  </div>
+                  <div className="flex w-full h-full flex-col items-start py-2 bg-violet-900">
+                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                      Filter by <br></br>category & time
+                    </h3>
+
+                  </div>
+
                 </div>
                 <div className="p-6 pt-0 text-muted-foreground text-sm">
                   You have 12 active projects and 3 pending tasks.
@@ -79,14 +83,20 @@ const ThirdPage = () => {
             </TabsContent>
             <TabsContent value="analytics">
               <div className="h-[400px] rounded-3xl border bg-card text-card-foreground shadow-sm">
-                <div className="flex flex-col space-y-1.5 p-6">
-                  <h3 className="text-2xl font-semibold leading-none tracking-tight">
-                    Analytics
-                  </h3>
-                  <p className={`text-sm text-muted-foreground ${outfit.className}`}>
-                    Track performance and user engagement metrics. Monitor
-                    trends and identify growth opportunities.
-                  </p>
+                <div className="flex h-full justify-center items-center p-8 bg-amber-300">
+                  <div className="flex !pl-10 !pt-8 relative w-[50%] h-full flex-col items-start py-2 bg-violet-300">
+                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                      Filter by <br></br>category & time
+                    </h3>
+
+                  </div>
+                  <div className="flex w-full pt-8 h-full flex-col items-start bg-violet-900">
+                    <h3 className={`${outfit.className} tracking-tight text-xl `}>
+                      Filter by <br></br>category & time
+                    </h3>
+
+                  </div>
+
                 </div>
                 <div className="p-6 pt-0 text-muted-foreground text-sm">
                   Page views are up 25% compared to last month.
